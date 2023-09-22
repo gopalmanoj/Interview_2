b36175690a6fb093d877993814cf29bcf8a68b46
Mishra <r78mishra@gmail.com>
2023-09-22 21:58:55 +0530

    word frequency count commited

--- WW_java_8/src/com/count/Count_word.java
+++ WW_java_8/src/com/count/Count_word.java
@@ -14,6 +14,26 @@ public class Count_word {
 	{
 		return IntStream.rangeClosed(2, num/2).noneMatch(p->num%p==0);
 	}
+	
+	public static void WordCount(String string)
+	{
+		String str[]=string.split("\\s");
+		Map<String,Integer> m=new LinkedHashMap<String, Integer>();
+		for(int i=0;i<str.length;i++)
+		{
+			System.out.println(str[i]);  
+			
+		if(m.containsKey(str[i]))
+		{
+			m.put(str[i], m.get(str[i])+1);
+		}
+		else
+		{
+			m.put(str[i], 1);
+		}
+		}
+		System.out.println(m); 
+	}
 
 	public static void main(String[] args) {
 		int num[]= {1,1,2,2,3,34,5,6,6,7,8,1,2,3,49,7};
@@ -29,6 +49,9 @@ public class Count_word {
 	List<Integer> primeNos=	number.stream().filter(Count_word::IsprimeNo).collect(Collectors.toList());
 	
 	System.out.println(primeNos); 
+	
+	String str="Welcome to the Bharat Welcome to the Bharat Welcome to the Bharat";
+	WordCount(str); 
 
 	}
 
