50e3b74946364e4edb7bb1df6215d4a2b2340099
Mishra <r78mishra@gmail.com>
2023-09-22 20:45:36 +0530

    Count_word class is updated

--- WW_java_8/src/com/count/Count_word.java
+++ WW_java_8/src/com/count/Count_word.java
@@ -1,9 +1,19 @@
 package com.count;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Count_word {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num[]= {1,1,2,2,3,34,5,6,6,7,8,1,2,3,49,7};
+		Map<Integer, Long> m=Arrays.stream(num).boxed().
+				collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new
+						,Collectors.counting()));
+		System.out.println(m); 
 
 	}
 
